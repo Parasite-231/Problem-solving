886639edf9150031b7d09e3fe10ca04bed79143e
Md Muktadir <70088622+Strelitzia-02@users.noreply.github.com>
2021-06-10 11:41:59 +0600

    Problem name->Sphere

---
+++ URI Online Judge Solutions/1011.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    double VOLUME,pi=3.14159,R;
+    scanf("%lf",&R);
+    VOLUME=(4.0/3.0)*pi*R*R*R;
+    printf("VOLUME = %.3f\n",VOLUME);
+    return 0;
+}
+
